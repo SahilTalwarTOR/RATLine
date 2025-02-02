81a2579f8b2f0f9b54bf4128be6ff1b6f3f61d66
SahilTalwarTOR <sahiltalwar2004@gmail.com>
2025-02-02 15:12:59 -0400

    Added better labels

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from itertools import count
 from os.path import exists
 
 import pandas as pd
@@ -6,6 +7,11 @@ import openpyxl
 from datetime import datetime
 import time
 import os
+import matplotlib.patches as mpatches
+
+pd.options.mode.chained_assignment = None  # Reduce warning overhead
+print("Enabled multi threading")
+
 
 #Setting up our config file
 while True:
@@ -42,13 +48,15 @@ df = pd.DataFrame()
 fig, ax = plt.subplots(figsize=(10,4))
 
 
-
+maxX = len(pd.ExcelFile(excel_name + ".xlsx").sheet_names)
+iterator = 0
 
 for x in pd.ExcelFile(excel_name + ".xlsx").sheet_names:
     excelFile = pd.read_excel(excel_name + ".xlsx", sheet_name=x)
     excelFile = excelFile.query('`base key`.isna() == False')
     excelFile = excelFile.filter(items=['base key', 'base t1adj', 'base t2adj', 'base Δt'])
-    print("Working on Sheet ", x)
+    print("Working on Sheet ", x, iterator, "/", maxX)
+    iterator += 1
 
     data = {
         'RAT_ID': [x] * len(excelFile),
@@ -74,6 +82,13 @@ for x in pd.ExcelFile(excel_name + ".xlsx").sheet_names:
 
 ax.set_yticks(range(len(rats)))
 ax.set_yticklabels(rats)
+ax.set_title("Timeline of RAT Behaviours")
+
+legend_patches = [mpatches.Patch(color=color, label=behavior) for behavior, color in colors.items()]
+
+ax.legend(handles=legend_patches, title="Letter Colours",loc='upper center', bbox_to_anchor=(0.5, -0.05),
+          fancybox=True, shadow=True, ncol=5)
+
 
 ax.grid(True, axis='x', linestyle='--', alpha=0.7)
 ax.set_ylim(-1, len(rats))  # Adjust to fit all rats
